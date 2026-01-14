dfecedbf4b38de374b68ad9a6e5a6d4662b1a6aa
Andrey Zharkikh <orbital.logistics@yandex.ru>
2026-01-14 13:26:30 +0300

    feat: 6 tests passing

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -117,8 +117,13 @@ function getPolynom(...coeffs) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let cache;
+
+  return function memoizer() {
+    if (!cache) cache = func();
+    return cache;
+  };
 }
 
 /**
